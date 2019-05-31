dd830ee9dafc08573c012779941333e096df25af
Jean Canales <jeancanales@jeans-macbook-pro.local>
2019-05-30 23:22:03 -0400

    Added button for simple emit function send.

--- ApiCallProduct/ViewController.swift
+++ ApiCallProduct/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import SocketIO
+
+
+
 
 class ViewController: UIViewController {
+    
+    @IBAction func btnClick(btn: UIButton){
+        self.socket.emit("test", 10);
+    }
+    //Intialize the SocketManager with URL and config with .log true. compress properties.
+    let manager = SocketManager(socketURL: URL(string: "http://localhost:5000")!, config: [.log(true), .compress])
+    var socket:SocketIOClient!
+    
+    var name: String?
+    var resetAck: SocketAckEmitter?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        socket = manager.defaultSocket
+        
+        socket.on("news"){ data, ack in
+            guard let dataReceived = data as Any? else { return }
+            
+            print(dataReceived);
+            
+        }
+        
+        
+    
+        
+        socket.connect();
         // Do any additional setup after loading the view, typically from a nib.
+        print("Here");
     }
-
+    
+    
 
 }
 
